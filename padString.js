dc3fd8e9fe0ffd68c2355532e0edb4cc8dc7c404
Shea Sollars <shea.sollars@gmail.com>
2016-04-20 04:11:25 -0700

    creates a string padding function with padStart and padEnd polyfills

---
+++ padString.js
@@ -0,0 +1,53 @@
+var padString = module.exports = function (targetString, length, padding, append) {
+	// If string is already at/over the limit
+	if (targetString.length >= length) {
+		// Just return the string as is
+		return targetString;
+	};
+
+	if (!padding) {
+		// Default padding is a space
+		padding = ' ';
+	}
+	else {
+		// Ensure padding with a String
+		padding = String(padding);
+	};
+
+	// If padding already brings us over our limit
+	if (padding.length + targetString.length >= length) {
+		// No need to repeat padding
+		var repeatTimes = 1;
+	}
+	else {
+		// Figure out how many chars until we reach the limit
+		var repeatTimes = length - targetString.length;
+		// Then figure out how many times we need to repeat the padding
+		repeatTimes = Math.ceil(repeatTimes / padding.length);
+	};
+
+	// Make the padding sufficient enough
+	padding = padding.repeat(repeatTimes);
+
+	// Appending...
+	if (append) {
+		// Just add the padding and cut off the excess
+		return (targetString + padding).slice(0, length);
+	};
+
+	// Figure out if and how much to trim
+	var trimAmount = (padding.length + targetString.length) - length;
+
+	// Trim if need and prepend the padding
+	return padding.slice(0, padding.length - trimAmount) + targetString;
+};
+
+// padStart polyfill
+String.prototype.padStart = function (length, padding) {
+	return padString(this, length, padding);
+};
+
+// padEnd polyfill
+String.prototype.padEnd = function (length, padding) {
+	return padString(this, length, padding, true);
+};
---
+++ test.js
@@ -0,0 +1,27 @@
+var padString = require('padString.js');
+
+var test = "test";
+
+console.log('\npadStart, target length of 10');
+console.log('1:  ' + test.padStart(10, "1"));
+console.log('2:  ' + test.padStart(10, "12"));
+console.log('3:  ' + test.padStart(10, "123"));
+console.log('4:  ' + test.padStart(10, "1234"));
+console.log('5:  ' + test.padStart(10, "12345"));
+console.log('6:  ' + test.padStart(10, "123456"));
+console.log('7:  ' + test.padStart(10, "1234567"));
+console.log('8:  ' + test.padStart(10, "12345678"));
+console.log('9:  ' + test.padStart(10, "123456789"));
+console.log('10: ' + test.padStart(10, "1234567890"));
+
+console.log('\npadEnd, target length of 10');
+console.log('1:  ' + test.padEnd(10, "1"));
+console.log('2:  ' + test.padEnd(10, "12"));
+console.log('3:  ' + test.padEnd(10, "123"));
+console.log('4:  ' + test.padEnd(10, "1234"));
+console.log('5:  ' + test.padEnd(10, "12345"));
+console.log('6:  ' + test.padEnd(10, "123456"));
+console.log('7:  ' + test.padEnd(10, "1234567"));
+console.log('8:  ' + test.padEnd(10, "12345678"));
+console.log('9:  ' + test.padEnd(10, "123456789"));
+console.log('10: ' + test.padEnd(10, "1234567890"));
